c72ba2d7a8dc67ea1381c7489b012d943f3da3a4
break <fengyingdian@126.com>
2021-10-21 20:50:35 +0800

    fix _removeFromList

--- src/RangeList.ts
+++ src/RangeList.ts
@@ -6,7 +6,7 @@
 /*   By: break <jixueqing@flipboard.cn>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 23:31:04 by break             #+#    #+#             */
-/*   Updated: 2021/10/21 12:36:20 by break            ###   ########.fr       */
+/*   Updated: 2021/10/21 20:50:17 by break            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,21 @@ class MyRangeList {
   }
 
   /**
-   * Adds a range to the list
+   * Add a range to the list
    * @param range {MyRange};
    */
   add(range: MyRange) {
     this._validateRange(range);
     this._insertIntoList(range);
+  }
+
+  /**
+   * Remove a range from the list
+   * @param range {MyRange};
+   */
+  remove(range: MyRange) {
+    this._validateRange(range);
+    this._removeFromList(range);
   }
 
   print() {
@@ -72,9 +81,9 @@ class MyRangeList {
       return this.rangeList.push(range);
     }
 
+    const [start1, end1] = range;
     while (iterator.i < iterator.size) {
       const currentRange = this.rangeList[iterator.i];
-      const [start1, end1] = range;
       const [start2, end2] = currentRange;
 
       // the range is on the RIGHT side of the currentRange
@@ -94,7 +103,8 @@ class MyRangeList {
       }
 
       // overlapped
-      //
+      // and the range's second value is larger than
+      // the current range's second value
       if (end1 > end2) {
         if (iterator.itersectStartIndex === -1) {
           iterator.itersectStartIndex = iterator.i;
@@ -114,38 +124,13 @@ class MyRangeList {
       return this.rangeList.splice(iterator.itersectStartIndex, iterator.i - iterator.itersectStartIndex + 1, newRange);
     }
 
-    if (iterator.i === iterator.size) {
-      if (iterator.itersectStartIndex === -1) {
-        return this.rangeList.push(range);
-      }
-
-      const newRange = this._merge(this.rangeList[iterator.itersectStartIndex], this._merge(this.rangeList[iterator.size - 1], range));
-      this.rangeList.splice(iterator.itersectStartIndex, (iterator.size - iterator.itersectStartIndex), newRange);
-      return;
+    if (iterator.itersectStartIndex === -1) {
+      return this.rangeList.push(range);
     }
+
+    const newRange = this._merge(this.rangeList[iterator.itersectStartIndex], this._merge(this.rangeList[iterator.size - 1], range));
+    this.rangeList.splice(iterator.itersectStartIndex, (iterator.size - iterator.itersectStartIndex), newRange);
   }
-
-  /**
-   * check out two validated ranges' relationship
-   * @param range {MyRange};
-   * @param currentRange {MyRange}
-   */
-  // _relationship(range: MyRange, currentRange: MyRange) {
-  //   const [start1, end1] = range;
-  //   const [start2, end2] = currentRange;
-
-  //   // left side
-  //   if (end1 < start2) return 1;
-  //   // right side
-  //   if (end2 < start1) return 2;
-
-  //   // overlaped
-  //   if (end1 <= end2) return 3;
-
-  //   // overlap and need to check next range's relationship
-  //   // if there exists next range
-  //   return 4;
-  // }
 
   /**
    * merge two ranges into one range
@@ -157,29 +142,123 @@ class MyRangeList {
     const [start2, end2] = currentRange;
     return [Math.min(start1, start2), Math.max(end1, end2)] as MyRange;
   }
+
+  /**
+   * remove the range from range list
+   * @param range {MyRange};
+   */
+  _removeFromList(range: MyRange) {
+    const iterator: {
+      i: number,
+      size: number,
+      itersectStartIndex: number,
+    } = {
+      i: 0,
+      size: this.rangeList.length,
+      itersectStartIndex: -1,
+    };
+
+    // if range list is empty, just return
+    if (iterator.size === 0) {
+      return;
+    }
+
+    const [start1, end1] = range;
+    while (iterator.i < iterator.size) {
+      const currentRange = this.rangeList[iterator.i];
+      const [start2, end2] = currentRange;
+
+      // the range is on the RIGHT side of the currentRange
+      if (end2 < start1) {
+        iterator.i++;
+        continue;
+      }
+
+      // the range is on the LEFT side of the currentRange
+      if (end1 < start2) {
+        if (iterator.itersectStartIndex === -1) {
+          return;
+        }
+
+        const [start3] = this.rangeList[iterator.itersectStartIndex];
+        if (start3 < start1) {
+          const newRange = [start3, start1] as MyRange;
+          return this.rangeList.splice(iterator.itersectStartIndex, iterator.i - iterator.itersectStartIndex + 1, newRange);
+        }
+      }
+
+      // overlapped
+      // and the range's second value is larger than
+      // the current range's second value
+      if (end1 > end2) {
+        if (iterator.itersectStartIndex === -1) {
+          iterator.itersectStartIndex = iterator.i;
+        }
+
+        iterator.i++;
+        continue;
+      }
+
+      // overlapped
+      const ranges = [] as MyRange[];
+      if (end1 < end2) {
+        ranges.push([end1, end2] as MyRange);
+      }
+
+      // just intersected with current range
+      if (iterator.itersectStartIndex === -1) {
+        if (start2 < start1) {
+          ranges.unshift([start2, start1] as MyRange);
+        }
+        return this.rangeList.splice(iterator.i, 1, ...ranges);
+      }
+
+      // has itersected with range befor current range
+      const [start3] = this.rangeList[iterator.itersectStartIndex];
+      if (start3 < start1) {
+        ranges.unshift([start3, start1] as MyRange);
+      }
+      return this.rangeList.splice(iterator.itersectStartIndex, iterator.i - iterator.itersectStartIndex + 1, ...ranges);
+    }
+
+    if (iterator.itersectStartIndex !== -1) {
+      const [start3] = this.rangeList[iterator.itersectStartIndex];
+      if (start3 < start1) {
+        const newRange = [start3, start1] as MyRange;
+        this.rangeList.splice(iterator.itersectStartIndex, iterator.i - iterator.itersectStartIndex + 1, newRange);
+      }
+    }
+  }
 }
 
 const rl = new MyRangeList();
 rl.add([1, 5]);
-rl.print();
+// rl.print();
 // Should display: [1, 5)
 rl.add([10, 20]);
-rl.print();
+// rl.print();
 // Should display: [1, 5) [10, 20)
 rl.add([20, 20]);
-rl.print();
+// rl.print();
 // Should display: [1, 5) [10, 20)
 rl.add([20, 21]);
-rl.print();
+// rl.print();
 // Should display: [1, 5) [10, 21)
 rl.add([2, 4]);
-rl.print();
+// rl.print();
 // Should display: [1, 5) [10, 21)
 rl.add([3, 8]);
+// rl.print();
+// Should display: [1, 8) [10, 21)
+rl.remove([10, 10]);
 rl.print();
-rl.add([-5, -4]);
+// Should display: [1, 8) [10, 21)
+rl.remove([10, 11]);
 rl.print();
-rl.add([50, 56]);
+// Should display: [1, 8) [11, 21)
+rl.remove([15, 17]);
 rl.print();
-rl.add([3, 58]);
+// Should display: [1, 8) [11, 15) [17, 21)
+rl.remove([3, 19]);
 rl.print();
+// Should display: [1, 3) [19, 21)
